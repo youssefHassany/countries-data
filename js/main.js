bfea3aa4ab658de98ab453be5bd3a6f5b1e1e7f0
Youssef <y.2003eg@gmail.com>
2023-03-24 20:19:43 +0200

    UI Done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let countryName = "Egypt";
+
+// fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     let myImg = document.createElement("img");
+//     myImg.src = data[0].flags.png;
+//     document.body.appendChild(myImg);
+//     console.log(data);
+//     console.log(data[0].flags.png);
+//   });
