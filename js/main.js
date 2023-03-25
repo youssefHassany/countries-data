5f05a6937c9d1c8696f1b53920aa3bd8d856f6cd
Youssef <y.2003eg@gmail.com>
2023-03-25 04:30:01 +0200

    final result

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-// let countryName = "Egypt";
+var btn = document.getElementById("my-btn");
+var container = document.querySelector(".data");
+let inp = document.getElementById("search-box");
 
-// fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let myImg = document.createElement("img");
-//     myImg.src = data[0].flags.png;
-//     document.body.appendChild(myImg);
-//     console.log(data);
-//     console.log(data[0].flags.png);
-//   });
+inp.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    btn.click();
+  }
+});
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let countryName = inp.value;
+
+  fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+    .then((response) => response.json())
+    .then((data) => {
+      let myImg = document.getElementById("img");
+      myImg.src = data[0].flags.png;
+      //   container.prepend(myImg);
+
+      let countryFullName = document.querySelector(".country-name");
+      countryFullName.innerHTML = data[0].altSpellings[1];
+
+      let continent = document.querySelector(".continent");
+      continent.innerHTML = data[0].continents;
+
+      let capital = document.querySelector(".capital");
+      capital.innerHTML = data[0].capital;
+
+      let population = document.querySelector(".population");
+      population.innerHTML = data[0].population;
+
+      console.log(data);
+      console.log(data[0].flags.png);
+    });
+});
